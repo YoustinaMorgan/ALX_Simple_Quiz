7a9ea9401720d0b9724bcc9974963b8cf0dc98e2
Youstina Morgan <Youstina.morgan1995@gmail.com>
2024-07-19 19:46:25 +0300

    last

--- calculator.js
+++ calculator.js
@@ -11,4 +11,11 @@ function divide(number1, number2) {
     if (number2 === 0 ) {
     return 'error';
     }
+    return number1 / number2
 }
+document.getElementById('add').addEventListener('click', function() {
+    const number1 = Parsefloat(getElementById('number1').value) || 0;
+    const number2 = parseFloat(getElementById('number2').value) || 0;
+    const result = add(number1,number2);
+    document.getElementById('result').textContent = result;
+})
